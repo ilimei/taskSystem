ac3ed3a690ab9d1314d6594189a9be770173dbbc
ilimei <514222057@qq.com>
2017-01-30 07:55:08 +0800

    增加文档页面

---
+++ jsx/page/project/Docs.jsx
@@ -0,0 +1,12 @@
+var React = require("react");
+
+/***
+ * React Component Docs create by ZhangLiwei at 20:21
+ */
+var Docs = React.createClass({
+    render: function () {
+        return <div className="Docs"></div>
+    }
+});
+
+module.exports = Docs;
