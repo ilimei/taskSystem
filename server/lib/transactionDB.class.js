f2241e848eef70ae752f9b4fa71d6fcd6b2eddaa
ilimei <514222057@qq.com>
2017-02-09 13:15:48 +0800

    增加状态判断

--- server/lib/transactionDB.class.js
+++ server/lib/transactionDB.class.js
@@ -8,6 +8,9 @@ module.exports=MakeClass({
     init:function(pool){
         this._pool=pool;
         this._executor=pool;
+        this._isTransaction=false;
+        this._isCommit=false;
+        this._isRollback=false;
     },
     startTransaction:function(){
         var _self=this;
@@ -16,8 +19,18 @@ module.exports=MakeClass({
                 if(err){
                     reject(err);
                 }else {
-                    _self._executor = conn;
-                    resolve(_self);
+                    conn.beginTransaction(function(err,result){
+                        if(err){
+                            conn.release();
+                            reject(err);
+                        }else {
+                            _self._executor = conn;
+                            _self._isTransaction=true;
+                            this._isCommit=false;
+                            this._isRollback=false;
+                            resolve(conn);
+                        }
+                    });
                 }
             });
         });
@@ -25,10 +38,36 @@ module.exports=MakeClass({
     query:function(sql,param,cb){
         this._executor.query(sql,param,cb);
     },
+    _check:function(){
+        if(!this._isTransaction){
+            throw new Error("must call the startTransaction method first!!");
+        }
+    },
     commit:function(){
-        this._executor.commit();
+        this._check();
+        if(!this._isCommit) {
+            if(this._isRollback){
+                throw new Error("this db has been rollback,cant commit")
+            }
+            this._isCommit=true;
+            this._executor.commit();
+        }
     },
     rollback:function(){
-        this._executor.rollback();
+        this._check();
+        if(!this._isRollback) {
+            if(this._isCommit){
+                throw new Error("this db has been commit,cant rollback")
+            }
+            this._isRollback=true;
+            this._executor.rollback();
+        }
+    },
+    release:function(){
+        if(this._isTransaction) {
+            this._pool.releaseConnection(this._executor);
+            this._isTransaction=false;
+            this._executor=this._pool;
+        }
     }
 },null,"TransactionDB");
