77d828c200fa7caa1bd5c9faeea5db080a7ab9bc
ilimei <514222057@qq.com>
2017-02-20 18:07:12 +0800

    添加ue editor

---
+++ jsx/ue/UEditor.jsx
@@ -0,0 +1,45 @@
+/***
+ * React Component UEditor create by ZhangLiwei at 15:49
+ */
+import * as React from "react";
+import "./ueditor.config";
+import "./ueditor.all";
+import "./zh-cn"
+
+
+class UEditor extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state={}
+    }
+
+    onReady(){
+        this.editor.setContent(this.props.value||"");
+    }
+
+    componentDidMount(){
+        this.editor=UE.getEditor(this.refs["container"]);
+        this.editor.on("ready",this.onReady.bind(this));
+        this.editor.on("contentChange",this.onChange.bind(this));
+        this.refs["container"].style.width="100%";
+    }
+
+    onChange(){
+        callAsFunc(this.props.change,[this.editor.getContent()]);
+    }
+    
+    componentWillUnmount(){
+        UE.delEditor(this.refs["container"]);
+    }
+
+    render() {
+        return <div className="UEditor">
+            <div className="editor_container" ref="container">
+            </div>
+        </div>
+    }
+}
+
+
+export default UEditor;
