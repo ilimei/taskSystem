45de4e39b0c3fdda0e1dd5605695ef1c0a56c844
ilimei <514222057@qq.com>
2017-02-21 13:20:59 +0800

    支持ctrl+v上传图片

--- jsx/page/project/dialog/MarkedDocDialog.jsx
+++ jsx/page/project/dialog/MarkedDocDialog.jsx
@@ -13,7 +13,7 @@ import Marked from "marked"
 import hljs  from '../../../marked/highlight';
 Marked.setOptions({
     renderer: new Marked.Renderer({
-        highlight: function(code, lang) {
+        highlight: function (code, lang) {
             if (lang)
                 return hljs.highlightAuto(code, [lang]).value;
             else
@@ -22,7 +22,7 @@ Marked.setOptions({
     }),
     gfm: true,
     breaks: true,
-    highlight: function(code, lang) {
+    highlight: function (code, lang) {
         if (lang)
             return hljs.highlightAuto(code, [lang]).value;
         else
@@ -36,60 +36,85 @@ class MarkedDocDialog extends React.Component {
      * default props value
      */
     props = {
-        title:"新增文档",
-        markdown:"",
-        data:null
+        title: "新增文档",
+        markdown: "",
+        data: null
     }
 
     /***
      * props types for helper text
      */
     static propTypes = {
-        title:React.PropTypes.string,
-        markdown:React.PropTypes.string,
-        data:React.PropTypes.object
+        title: React.PropTypes.string,
+        markdown: React.PropTypes.string,
+        data: React.PropTypes.object
     }
 
     constructor(props) {
         super(props);
-        var html=props.data?Marked(props.data.content):"";
+        var html = props.data ? Marked(props.data.content) : "";
         this.state = {
-            onHide:null,
-            title:props.data?props.data.title:"",
-            markdown:props.data?props.data.content:"",
-            html:{__html:html}
+            onHide: null,
+            title: props.data ? props.data.title : "",
+            markdown: props.data ? props.data.content : "",
+            html: {__html: html}
         }
     }
 
-    handleChange(value){
-        this.state.markdown=value;
-        this.state.html={__html:Marked(value)};
-        ReactDOM.findDOMNode(this.refs["div"]).innerHTML=this.state.html.__html;
+    handleChange(value) {
+        this.state.markdown = value;
+        this.state.html = {__html: Marked(value)};
+        ReactDOM.findDOMNode(this.refs["div"]).innerHTML = this.state.html.__html;
     }
 
-    handleChangeTitle(v){
-        this.state.title=v;
+    handleChangeTitle(v) {
+        this.state.title = v;
     }
 
-    cancel(){
+    cancel() {
         this.refs["modal"].hide();
     }
 
-    ok(){
-        callAsFunc(this.props.onOk,[{
-            title:this.state.title,
-            content:this.state.markdown
+    ok() {
+        callAsFunc(this.props.onOk, [{
+            title: this.state.title,
+            content: this.state.markdown
         }]);
         this.cancel();
+    }
+
+    parseImage(e) {
+        var me = this;
+        var clipboard = e.clipboardData;
+        var type = clipboard.items[0].type;
+        if (type.match(/image/)) {
+            var blob = clipboard.items[0].getAsFile();
+            var formData=new FormData();
+            formData.append("avatar",blob,"test.png");
+            PostFormData("api/upload/avatar",formData)
+                .then(function(data){
+                    if(data.success){
+                        me.refs["editor"].replaceSelection("![title](/uploads/" + data.result + ")");
+                    }else{
+                        alert(data.result)
+                    }
+                    console.dir(data);
+                }).catch(function(e){
+                console.dir(e);
+            });
+        }
+        e.preventDefault();
+        e.stopPropagation();
     }
 
     render() {
         return <Modal className="MarkedDocDialog" ref="modal" lg title={this.props.title} onHide={this.state.onHide}>
             <Input name="标题" value={this.state.title} change={this.handleChangeTitle.bind(this)}/>
             <Split>
-                <ResizeBox className="CodeEditorWrap" width={200}>
-                    <CodeEditor mode="text/x-markdown"
+                <ResizeBox className="CodeEditorWrap" width={400}>
+                    <CodeEditor ref="editor" mode="text/x-markdown"
                                 value={this.state.markdown}
+                                onPaste={this.parseImage.bind(this)}
                                 onChange={this.handleChange.bind(this)} lineWrapping
                                 height="100%"/>
                 </ResizeBox>
