cb3f5fc3a66e4fc871b472a5e365e773ae75ee65
ilimei <514222057@qq.com>
2017-02-21 13:21:44 +0800

    增加replaceSelection和setValue方法

--- jsx/codeMirror/CodeEditor.jsx
+++ jsx/codeMirror/CodeEditor.jsx
@@ -11,54 +11,62 @@ class CodeEditor extends React.Component {
      * default props value
      */
     props = {
-        value:"",
-        mode:"text/x-sql",
-        height:"100%"
+        value: "",
+        mode: "text/x-sql",
+        height: "100%"
     }
 
     /***
      * props types for helper text
      */
     static propTypes = {
-        value:React.PropTypes.string,
-        mode:React.PropTypes.string,
-        height:React.PropTypes.string|React.PropTypes.number,
-        onChange:React.PropTypes.func,
-        onKeyDown:React.PropTypes.func,
-        onPaste:React.PropTypes.func
+        value: React.PropTypes.string,
+        mode: React.PropTypes.string,
+        height: React.PropTypes.string | React.PropTypes.number,
+        onChange: React.PropTypes.func,
+        onKeyDown: React.PropTypes.func,
+        onPaste: React.PropTypes.func
     }
 
     constructor(props) {
         super(props);
         this.state = {
-            value:this.props.value||""
+            value: this.props.value || ""
         }
     }
 
-    componentWillReceiveProps(nextProps){
-        if(nextProps.value){
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.value) {
             this.editor.setValue(nextProps.value);
-            this.state.value=nextProps.value;
+            this.state.value = nextProps.value;
         }
     }
-    
-    componentDidMount(){
+
+    replaceSelection(str) {
+        this.editor.getDoc().replaceSelection(str);
+    }
+
+    setValue(value) {
+        this.editor.setValue(value);
+    }
+
+    componentDidMount() {
         this.editor = CodeMirror.fromTextArea(this.refs.textarea, {
             lineNumbers: true,
-            lineWrapping:this.props.lineWrapping,
-            mode: this.props.mode||"text/x-sql",
-            theme:"eclipse",
-            value:this.state.value
+            lineWrapping: this.props.lineWrapping,
+            mode: this.props.mode || "text/x-sql",
+            theme: "eclipse",
+            value: this.state.value
         });
-        this.editor.on("change",cm=>{
-            this.state.value=cm.getValue();
-            callAsFunc(this.props.onChange,[this.state.value]);
+        this.editor.on("change", cm => {
+            this.state.value = cm.getValue();
+            callAsFunc(this.props.onChange, [this.state.value]);
         });
     }
-    
+
     render() {
         return <div className="CodeEditor" onKeyDown={this.props.onKeyDown} onPaste={this.props.onPaste}
-                    style={{paddingTop:"1px",height:this.props.height||"auto"}}>
+                    style={{paddingTop: "1px", height: this.props.height || "auto"}}>
             <textarea ref="textarea" value={this.state.value}/>
         </div>
     }
