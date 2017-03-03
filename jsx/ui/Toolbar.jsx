c5c7429e8afbe9bd3a9eef6dbfef5347eff45486
ilimei <514222057@qq.com>
2017-03-03 13:42:36 +0800

    增加显示新消息的功能

--- jsx/ui/Toolbar.jsx
+++ jsx/ui/Toolbar.jsx
@@ -6,6 +6,7 @@ var React = require("react");
 var TaskList=require("./Tasklist");
 var LinkFunc=require("../lib/router/LinkFunc");
 import CreateTask from "../page/dialog/CreateTask";
+console.dir(process);
 
 var Toolbar = React.createClass({
     getInitialState:function(){
@@ -19,7 +20,9 @@ var Toolbar = React.createClass({
             data:[
                 {text:"创建项目",icon:"coding-project",click:this.createProject},
                 {text:"创建任务",icon:"icon-tasks",click:this.createTask}
-            ]
+            ],
+            unread:0,
+            msgList:[]
         }
     },
     createTask(){
@@ -45,10 +48,24 @@ var Toolbar = React.createClass({
     jumpToMyTask:function(){
         LinkFunc("/user/tasks/pending");
     },
+    selectMsgList:function(msg){
+        Ajax("api/msg/read",{
+            id:msg.id
+        },data=>{
+            this.loadUnRead();
+            LinkFunc(`/project/${msg.project_id}/tasks/detail/${msg.task_id}`);
+        });
+    },
+    loadUnRead:function(){
+        Ajax("api/msg/unread",{page:1,rows:5},function(data){
+            this.setState({unread:data.data[0],msgList:data.data[1]});
+        },this);
+    },
     componentDidMount:function(){
         cacheAjax("api/user/getLoginInfo",{},function (data) {
             this.setState({user:data});
         },this);
+        this.loadUnRead();
     },
     render: function() {
         var img="/headicon/02.png";
@@ -56,12 +73,23 @@ var Toolbar = React.createClass({
         if(user.avatar){
             img=user.avatar;
         }
+        var cls=CS({
+            "item":true,
+            "msgNew":this.state.unread>0
+        });
         return (
             <div className="Toolbar">
                 <div className="right-menu">
                     <span className="item" onClick={this.jumpToMyTask}>
-						<i className="icon-bell-alt"/>
-					</span>
+                        <i className="icon-tasks"/>
+                    </span>
+                    <DropIcon noDrop={this.state.unread<=0} className={cls} icon="icon-bell-alt">
+                        <TaskList data={this.state.msgList}
+                                  onClick={this.selectMsgList}
+                                  textField="content">
+                            <a className="more" href="#">查看更多通知》</a>
+                        </TaskList>
+					</DropIcon>
                     <DropIcon className="item" icon="icon-plus">
 						<TaskList data={this.state.data}/>
 					</DropIcon>
